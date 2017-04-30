d31fe9b7be9522339bb2440e2a519c97e906e01c
Randy Ferrer <randfer@gmail.com>
2017-04-29 23:09:42 -0400

    Lesson 7 complete

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-// class SelectLanguage extends React.Component {
-//   render() {
-//     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
-//     return (
-//       <ul className="languages">
-//         {languages.map((lang) => {
-//           return (
-//             <li
-//               style={lang === this.props.selectedLanguage ? { color: '#d0021b' } : null}
-//               onClick={this.props.onSelect.bind(null, lang)}
-//               key={lang}
-//             >
-//               {lang}
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     );
-//   }
-// }
-
-// From Class to Stateless Functional Component
+import api from '../utils/api';
 
 function SelectLanguage(props) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -45,26 +22,71 @@ function SelectLanguage(props) {
   );
 }
 
+function RepoGrid(props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map((repo, index) => {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={`Avatar for ${repo.owner.login}`}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        );
+      })
+      }
+    </ul>
+  );
+}
+
+RepoGrid.PropTypes = {
+  repos: PropTypes.array.isRequired
+};
+
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired
-}
+};
 
 class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
+  componentDidMount() {
+    // Ajax
+    this.updateLanguage(this.state.selectedLanguage);
+  }
   updateLanguage(lang) {
     this.setState(() => {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       };
     });
+
+    api.fetchPopularRepos(lang)
+      .then((repos) => {
+        this.setState(() => {
+          return {
+            repos
+          };
+        });
+      });
   }
 
   render() {
@@ -75,6 +97,10 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
+        {!this.state.repos
+          ? <p>LOADING</p>
+          : <RepoGrid repos={this.state.repos} />}
+
       </div>
     );
   }
---
+++ lesson-7.md
@@ -0,0 +1,7 @@
+## React Life Cycle Events
+#2 Main Categories
+ - When a component gets mounted to the DOM and unmounted
+ - When a component receives new data
+ 
+ # Continue app making Ajax Requests in React
+ - npm install --save axios
